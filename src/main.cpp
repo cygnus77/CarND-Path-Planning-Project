3703cc46351c32f9cf5c78f0a2156cec15487957
Anand Dinakar <ananddinakar@gmail.com>
2017-07-26 20:04:56 -0400

    first attempt at lane following

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,16 @@
 #include <vector>
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
+#include "Eigen-3.3/Eigen/Dense"
 #include "json.hpp"
 
 using namespace std;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+#define SPEED_LIMIT 22.352  // m/s = 50 mph
+#define TIME_TO_MAX 10      // 0 to 50 in 20 sec
+#define MPH2MS  0.447027269
 
 // for convenience
 using json = nlohmann::json;
@@ -159,6 +166,65 @@ vector<double> getXY(double s, double d, vector<double> maps_s, vector<double> m
 
 }
 
+vector<double> JMT(vector< double> start, vector <double> end, double T)
+{
+    /*
+    Calculate the Jerk Minimizing Trajectory that connects the initial state
+    to the final state in time T.
+
+    INPUTS
+
+    start - the vehicles start location given as a length three array
+        corresponding to initial values of [s, s_dot, s_double_dot]
+
+    end   - the desired end state for vehicle. Like "start" this is a
+        length three array.
+
+    T     - The duration, in seconds, over which this maneuver should occur.
+
+    OUTPUT 
+    an array of length 6, each value corresponding to a coefficent in the polynomial 
+    s(t) = a_0 + a_1 * t + a_2 * t**2 + a_3 * t**3 + a_4 * t**4 + a_5 * t**5
+
+    EXAMPLE
+
+    > JMT( [0, 10, 0], [10, 10, 0], 1)
+    [0.0, 10.0, 0.0, 0.0, 0.0, 0.0]
+    */
+    double tsq = T * T;
+    double tcu = tsq * T;
+    MatrixXd Tmat(3,3);
+    Tmat << tcu, tsq*tsq, tcu*tsq, 3*tsq, 4*tcu, 5*tsq*tsq, 6*T, 12*tsq, 20*tcu;
+    
+    double Sf = end[0], Sf_d = end[1], Sf_dd = end[2];
+    double Si = start[0], Si_d = start[1], Si_dd = start[2];
+    MatrixXd Sfmat(3,1);
+    Sfmat << Sf - (Si + Si_d*T + 0.5*Si_dd*T*T), Sf_d - (Si_d + Si_dd*T), Sf_dd - Si_dd;
+    
+    MatrixXd alpha = Tmat.inverse() * Sfmat;
+    
+    return {Si, Si_d, 0.5*Si_dd, alpha(0), alpha(1), alpha(2)};
+    
+}
+
+double poly_eval(const vector<double>& coeffs, double x)
+{
+  double result = 0;
+  for(int i = 0; i < coeffs.size(); i++)
+  {
+    result += coeffs[i] * pow(x, i);
+  }
+  return result;
+}
+
+double velocity_at(double x1, double y1, double x2, double y2, double t)
+{
+  double vel = sqrt( (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1) ) / t;
+  std::cout << "vel(["<<x1<<","<<y1<<"],["<<x2<<","<<y2<<"])="<<vel<<std::endl;
+  return vel;
+}
+
+
 int main() {
   uWS::Hub h;
 
@@ -171,9 +237,7 @@ int main() {
 
   // Waypoint map to read from
   string map_file_ = "../data/highway_map.csv";
-  // The max s value before wrapping around the track back to 0
-  double max_s = 6945.554;
-
+  
   ifstream in_map_(map_file_.c_str(), ifstream::in);
 
   string line;
@@ -198,6 +262,12 @@ int main() {
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
+
+    // The max s value before wrapping around the track back to 0
+    double max_s = 6945.554;
+    double max_pts = 100;
+
+
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -222,6 +292,7 @@ int main() {
           	double car_d = j[1]["d"];
           	double car_yaw = j[1]["yaw"];
           	double car_speed = j[1]["speed"];
+            double car_speed_ms = car_speed * MPH2MS;
 
           	// Previous path data given to the Planner
           	auto previous_path_x = j[1]["previous_path_x"];
@@ -240,6 +311,84 @@ int main() {
 
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+            int path_size = previous_path_x.size();
+
+            for(int i = 0; i < path_size; i++)
+            {
+                next_x_vals.push_back(previous_path_x[i]);
+                next_y_vals.push_back(previous_path_y[i]);
+            }
+
+            if(path_size < 50) {
+
+              std::cout << "telemetry " << path_size << "; car_speed_ms: " << car_speed_ms << "; car(s,d):" << car_s << "," << car_d << "; end(s,d):" << end_path_s << "," << end_path_d << std::endl;
+
+              double s, d, a, u;
+              if(path_size == 0)
+              {
+                s = car_s;
+                d = car_d;
+                a = 0;
+                u = 0;
+              }
+              else
+              {
+                s = end_path_s;
+                d = end_path_d;
+
+                /*
+                // velocity at s
+                u = velocity_at(previous_path_x[path_size-1], previous_path_y[path_size-1], previous_path_x[path_size-2], previous_path_y[path_size-2], 0.02);
+                double u0 = velocity_at(previous_path_x[path_size-2], previous_path_y[path_size-2], previous_path_x[path_size-3], previous_path_y[path_size-3], 0.02);
+                a = (u - u0) / 0.02;
+                */
+
+                
+                double t = path_size * 0.02; // num path entries * 20ms
+                a = 2 * (end_path_s - car_s - car_speed_ms*t)/(t*t);
+                u = car_speed_ms + (a * t);
+                
+              }
+
+              // Calculate trajectory
+              
+              double Ta = abs((SPEED_LIMIT - u) * TIME_TO_MAX / SPEED_LIMIT);
+              double newa = (SPEED_LIMIT - u) / Ta;
+              double v = u + newa * Ta;
+              double T = (max_pts-path_size)*0.02;
+              double final_s = s, final_v, final_a;
+              if(T < Ta){
+                final_s += u*T + 0.5*newa*T*T;
+                final_v = u + newa*T;
+                final_a = newa;
+              } else {
+                final_s += u*Ta + 0.5*newa*Ta*Ta;
+                final_s += v * (T-Ta);
+                final_v = v;
+                final_a = 0;
+              }
+              
+
+              vector<double> Si = { s, u, a };
+              vector<double> Sf = { final_s, final_v, final_a };
+
+              std::cout << "T:" << T << "; Ta:" << Ta << "; newa:" << newa << std::endl;
+              std::cout << "s,u,a:" << s << ", " << u << ", " << a << std::endl;
+              std::cout << "sf,vf,af:" << final_s << ", " << final_v << ", " << final_a << std::endl;
+              
+              vector<double> coeffs = JMT(Si, Sf, T);
+
+              double t = 0;
+              for(int i = 0; i < max_pts-path_size; i++, t += 0.02)
+              {    
+                s = poly_eval(coeffs, t);
+                if(s > max_s) s -= max_s;
+                vector<double> xy = getXY(s, d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+                next_x_vals.push_back(xy[0]);
+                next_y_vals.push_back(xy[1]);
+              }
+            }
+
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
